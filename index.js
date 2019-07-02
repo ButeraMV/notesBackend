ab2c5a549d4ca5411bb6ed2f69e4113a4f061abd
Boots2291 <buteramv@gmail.com>
2019-07-02 11:25:27 -0400

    notes backend

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
+
 const bodyParser = require('body-parser')
-const cors = require('cors')
 
 app.use(bodyParser.json())
-app.use(cors())
+app.use(express.static('build'))
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
 
 let notes = [
   {
@@ -28,35 +38,22 @@ let notes = [
 ]
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>')
-})
-
-app.get('/notes', (req, res) => {
-  res.json(notes)
-})
-
-app.get('/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
-    res.json(note)
-  } else {
-    res.status(404).end()
-  }
+  res.send('<h1>Hello World!</h1>')
 })
 
 const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
   return maxId + 1
 }
 
-app.post('/notes', (req, res) => {
-  const body = req.body
+app.post('/notes', (request, response) => {
+  const body = request.body
 
   if (!body.content) {
-    return res.status(400).json({ 
-      error: 'content missing' 
+    return response.status(400).json({
+      error: 'content missing'
     })
   }
 
@@ -69,17 +66,37 @@ app.post('/notes', (req, res) => {
 
   notes = notes.concat(note)
 
-  res.json(note)
+  response.json(note)
 })
 
-app.delete('/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
+app.get('/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
   notes = notes.filter(note => note.id !== id)
 
-  res.status(204).end()
+  response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
